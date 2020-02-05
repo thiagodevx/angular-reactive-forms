dccfbd9449d36d5d8234f580ca333e0d590a49e2
thiagodevx <thiago.dev.x@gmail.com>
2020-02-05 14:15:13 -0300

    code simplification

--- APM/src/app/products/product-edit.guard.ts
+++ APM/src/app/products/product-edit.guard.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { CanDeactivate } from '@angular/router';
-import { Observable } from 'rxjs';
 
 import { ProductEditComponent } from './product-edit.component';
 
@@ -8,7 +7,7 @@ import { ProductEditComponent } from './product-edit.component';
   providedIn: 'root'
 })
 export class ProductEditGuard implements CanDeactivate<ProductEditComponent> {
-  canDeactivate(component: ProductEditComponent): Observable<boolean> | Promise<boolean> | boolean {
+  canDeactivate(component: ProductEditComponent): boolean {
     if (component.productForm.dirty) {
       const productName = component.productForm.get('productName').value || 'New Product';
       return confirm(`Navigate away and lose all changes to ${productName}?`);
